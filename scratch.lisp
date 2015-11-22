b165ba3c9d685698b92ebc4e125f8ebc8773b9c2
Renee Klawitter <deepestthought42@gmail.com>
2015-11-21 21:57:04 -0800

    changed timeouts in c library

--- scratch.lisp
+++ scratch.lisp
@@ -1,9 +1,24 @@
 (in-package #:vxi11)
 
 
-#+nil
-(query-command "*IDN?" :ip-address "titangpib1.triumf.ca" :instrument-name "gpib0,8")
 
+(defun test ()
+  (let* ((sem (bt-sem:make-semaphore))
+	 (thread (bt:make-thread
+		 #'(lambda ()
+		     (query-command "*IDN?" :ip-address "titangpib1.triumf.ca" :instrument-name "gpib0,8")
+		     (bt-sem:signal-semaphore sem)))))
+    (if (not (bt-sem:wait-on-semaphore sem :timeout 2))
+	(progn
+	  (bt:destroy-thread thread)
+	  (format t "Killed thread")))))
+
+
+(test)
+
+(time
+ (utils:with-destroyed-after (1 :name "test")
+   (values 1 2)))
 
 
 
